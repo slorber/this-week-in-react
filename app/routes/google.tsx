2556f06c2a68f5d6075e3727f2119dc16efc4fe5
sebastienlorber <lorber.sebastien@gmail.com>
2022-03-31 00:05:26 +0200

    GA conversion tracker

--- app/routes/google.tsx
+++ app/routes/google.tsx
@@ -1,11 +1,13 @@
 import Index, { createActionFunction, createMeta } from "./index";
 import { MetaFunction } from "remix";
 
+// Global site tag (gtag.js) - Google Ads: 10876347443
 const GoogleTracker = `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-224278710-1');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-10876347443');
 `;
 
 declare global {
@@ -25,10 +27,16 @@ export const action = createActionFunction({
 export default function IndexTwitter() {
   return (
     <>
-      <Index onSubscribe={(email) => {}} />
+      <Index
+        onSubscribe={(email) => {
+          window.gtag("event", "conversion", {
+            send_to: "AW-10876347443/tOVbCLiFq64DELPIn8Io",
+          });
+        }}
+      />
       <script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=UA-224278710-1"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-10876347443"
       />
       <script
         dangerouslySetInnerHTML={{
