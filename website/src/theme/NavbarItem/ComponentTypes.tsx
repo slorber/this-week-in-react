f008ede6009727750fe4e8851a1033388670f14b
sebastienlorber <lorber.sebastien@gmail.com>
2022-08-30 10:44:22 +0200

    improve navbar/footer icons

---
+++ website/src/theme/NavbarItem/ComponentTypes.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import ComponentTypes from "@theme-original/NavbarItem/ComponentTypes";
+import DefaultNavbarItem from "@theme/NavbarItem/DefaultNavbarItem";
+import { useBaseUrlUtils } from "@docusaurus/useBaseUrl";
+
+// TODO we should be able to use "children" instead of "label"?
+function TwitterThread(props) {
+  const { withBaseUrl } = useBaseUrlUtils();
+  const label = (
+    <span style={{ display: "flex", alignItems: "center" }}>
+      <img
+        alt="Twitter icon"
+        src={withBaseUrl("/svg/twitter.svg")}
+        width={20}
+        height={20}
+        style={{ marginRight: "0.2rem" }}
+      />
+      {props.label}
+    </span>
+  );
+  return <DefaultNavbarItem {...props} label={label} />;
+}
+
+console.log("test");
+
+export default {
+  ...ComponentTypes,
+  "custom-twitterThread": TwitterThread,
+};
