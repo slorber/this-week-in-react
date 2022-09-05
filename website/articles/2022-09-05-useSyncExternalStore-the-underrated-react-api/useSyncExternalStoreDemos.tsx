89166a5cc245d86d35ea4f15b6eff13b05e3fb04
sebastienlorber <lorber.sebastien@gmail.com>
2022-09-05 11:31:27 +0200

    stable blog post demos

---
+++ website/articles/2022-09-05-useSyncExternalStore-the-underrated-react-api/useSyncExternalStoreDemos.tsx
@@ -0,0 +1,136 @@
+import React, { ReactNode, useCallback } from "react";
+import { useSyncExternalStore as useSyncExternalStoreShim } from "use-sync-external-store/shim";
+import { useLocation, useHistory } from "@docusaurus/router";
+import Link from "@docusaurus/Link";
+import BrowserWindow from "@site/src/components/BrowserWindow";
+import styles from "./index.module.css";
+import clsx from "clsx";
+
+function useSyncExternalStore<Snapshot>(
+  subscribe: (onStoreChange: () => void) => () => void,
+  getSnapshot: () => Snapshot,
+  getServerSnapshot?: () => Snapshot
+): Snapshot {
+  return useSyncExternalStoreShim(subscribe, getSnapshot, getServerSnapshot);
+}
+
+function RenderIndicator() {
+  return <span className={clsx(styles.rerender)}>Render</span>;
+}
+
+function RenderBox({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) {
+  const renderKey = Math.random();
+
+  const border = "solid grey 2px";
+  const borderRadius = 5;
+
+  return (
+    <div
+      key={renderKey}
+      className={clsx(
+        styles.component
+        // "bg-gray-900 rounded text-white p-4 border-2 border-gray-400 relative mt-4"
+      )}
+      style={{
+        position: "relative",
+        border,
+        borderRadius: 5,
+        boxShadow: "var(--ifm-global-shadow-md)",
+        marginBottom: "1rem",
+        overflow: "hidden",
+      }}
+    >
+      <div>
+        <span
+          style={{
+            fontSize: 24,
+            fontWeight: "bold",
+            borderWidth: 2,
+            borderBottom: border,
+            borderRight: border,
+            borderBottomRightRadius: borderRadius,
+            padding: "0.4rem",
+          }}
+        >
+          {title}
+        </span>
+        <RenderIndicator />
+      </div>
+      <div style={{ marginTop: "0.5rem", padding: "0.5rem" }}>{children}</div>
+    </div>
+  );
+}
+
+function CurrentPathname() {
+  const { pathname } = useLocation();
+  return <RenderBox title="Pathname">{pathname}</RenderBox>;
+}
+
+function CurrentHash() {
+  const { hash } = useLocation();
+  return <RenderBox title="Hash">{hash || "undefined"}</RenderBox>;
+}
+
+function Links() {
+  return (
+    <RenderBox title="Links">
+      <Link to="#link1">#link1</Link>
+      <Link to="#link2" style={{ marginLeft: "2rem" }}>
+        #link2
+      </Link>
+      <Link to="#link3" style={{ marginLeft: "2rem" }}>
+        #link3
+      </Link>
+    </RenderBox>
+  );
+}
+
+export const App = React.memo(function App() {
+  return (
+    <BrowserWindow>
+      <CurrentPathname />
+      <CurrentHash />
+      <Links />
+    </BrowserWindow>
+  );
+});
+
+type History = ReturnType<typeof useHistory>;
+
+function useHistorySelector<Result>(selector: (history: History) => Result) {
+  const history = useHistory();
+
+  // Memoizing the callback is important, otherwise React re-subscribes
+  const subscribe = useCallback(
+    (onStoreChange) => {
+      // Notify React when external store changes
+      const unsubscribe = history.listen(onStoreChange);
+      // Give React a way to unsubscribe
+      return () => unsubscribe();
+    },
+    [history]
+  );
+
+  return useSyncExternalStore(subscribe, () => selector(history));
+}
+
+function CurrentPathnameFixed() {
+  const pathname = useHistorySelector((history) => history.location.pathname);
+  return <RenderBox title="Pathname">{pathname}</RenderBox>;
+}
+
+export const AppFixed = React.memo(function App() {
+  return (
+    <BrowserWindow>
+      <CurrentPathnameFixed />
+      <CurrentHash />
+      <Links />
+    </BrowserWindow>
+  );
+});
