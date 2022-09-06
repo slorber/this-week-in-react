5a14584d39e735d6e7856cb153475b8c7783a119
sebastienlorber <lorber.sebastien@gmail.com>
2022-09-06 14:54:20 +0200

    auto add retweet/subscribe CTAs to blog posts

---
+++ website/src/theme/BlogPostItem/Content/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useBlogPost } from "@docusaurus/theme-common/internal";
+import type ContentType from "@theme/BlogPostItem/Content";
+import Content from "@theme-original/BlogPostItem/Content";
+import SubscribeFormEmbed from "@site/src/theme/MDXComponents/SubscribeFormEmbed";
+import useDocusaurusContext from "@docusaurus/core/lib/client/exports/useDocusaurusContext";
+import Link from "@docusaurus/Link";
+
+type Props = React.ComponentProps<typeof ContentType>;
+
+function TwitterThreadCTA({ twitterThreadUrl }: { twitterThreadUrl: string }) {
+  const { i18n } = useDocusaurusContext();
+  const text =
+    i18n.currentLocale === "fr"
+      ? "Tu as aimé cet article ?"
+      : "Did you like this article?";
+
+  const linkText = i18n.currentLocale === "fr" ? "Retweet le!" : "Retweet it!";
+  return (
+    <div style={{ margin: "1rem 0" }}>
+      ❤️️ {text}{" "}
+      <Link to={twitterThreadUrl}>
+        <strong>{linkText}</strong>
+      </Link>{" "}
+      🙏
+    </div>
+  );
+}
+
+export default function ContentWrapper(props) {
+  const { frontMatter } = useBlogPost();
+  return (
+    <>
+      <Content {...props} />
+      {frontMatter.twitterThreadUrl && (
+        <TwitterThreadCTA twitterThreadUrl={frontMatter.twitterThreadUrl} />
+      )}
+      <SubscribeFormEmbed />
+    </>
+  );
+}
