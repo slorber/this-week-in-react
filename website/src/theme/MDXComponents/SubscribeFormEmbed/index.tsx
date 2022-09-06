0b79c34f89dc0e64938c4a7a0465c90cf34680be
sebastienlorber <lorber.sebastien@gmail.com>
2022-09-06 14:07:28 +0200

    start translate

--- website/src/theme/MDXComponents/SubscribeFormEmbed/index.tsx
+++ website/src/theme/MDXComponents/SubscribeFormEmbed/index.tsx
@@ -2,12 +2,18 @@ import React, { ComponentProps, ReactNode } from "react";
 import clsx from "clsx";
 import SubscribeForm from "@site/src/components/SubscribeForm";
 import styles from "./index.module.css";
+import useDocusaurusContext from "@docusaurus/core/lib/client/exports/useDocusaurusContext";
 
 export default function SubscribeFormEmbed(props: ComponentProps<"div">) {
+  const { i18n } = useDocusaurusContext();
+  const text =
+    i18n.currentLocale === "fr"
+      ? "Ne manque pas le prochain article !"
+      : "Don't miss the next article!";
   return (
     <div {...props} className={clsx("card", styles.subscribeFormEmbed, props)}>
       <p>
-        <strong>Don't miss the next article!</strong>
+        <strong>{text}</strong>
       </p>
       <SubscribeForm />
     </div>
