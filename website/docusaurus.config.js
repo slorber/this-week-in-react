7126299cbf2e8a94906d7cf188dd94a442ad8715
sebastienlorber <lorber.sebastien@gmail.com>
2022-10-03 11:45:03 +0200

    add gtag config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -42,6 +42,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-WMDM3YHXPR",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
