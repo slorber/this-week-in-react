820bc0cc5b8b886d1c7573141b65deb2266f80a7
sebastienlorber <lorber.sebastien@gmail.com>
2022-12-10 19:04:23 +0100

    add gtm datalayer

--- website/plugins/gtm/index.js
+++ website/plugins/gtm/index.js
@@ -31,6 +31,10 @@ module.exports = function pluginGoogleTagManager(context, options) {
           },
           {
             tagName: "script",
+            innerHTML: `window.dataLayer = window.dataLayer || [];`,
+          },
+          {
+            tagName: "script",
             innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
