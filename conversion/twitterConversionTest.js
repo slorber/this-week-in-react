decc70d5289b8f563ead1220880e34b4894b2038
sebastienlorber <lorber.sebastien@gmail.com>
2023-01-24 09:56:34 +0100

    poc of using Twitter ads CAPI for conversions

--- conversion/twitterConversionTest.js
+++ conversion/twitterConversionTest.js
@@ -1,4 +1,10 @@
 const Twitter = require("twitter");
+const TwitterLite = require("twitter-lite");
+const crypto = require("crypto");
+
+function sha256(input) {
+  return crypto.createHash("sha256").update(input).digest("hex");
+}
 
 function readEnvVariable(name, defaultValue) {
   const value = process.env[name];
@@ -16,6 +22,18 @@ const client = new Twitter({
   consumer_secret: readEnvVariable("TWIR_TWITTER_API_SECRET"),
   access_token_key: readEnvVariable("TWIR_TWITTER_ACCESS_TOKEN"),
   access_token_secret: readEnvVariable("TWIR_TWITTER_ACCESS_TOKEN_SECRET"),
+  // bearer_token: readEnvVariable("TWIR_TWITTER_BEARER_TOKEN"),
+});
+
+const client2 = new TwitterLite({
+  subdomain: "ads-api",
+  version: "11",
+  extension: false,
+  consumer_key: readEnvVariable("TWIR_TWITTER_API_KEY"),
+  consumer_secret: readEnvVariable("TWIR_TWITTER_API_SECRET"),
+  access_token_key: readEnvVariable("TWIR_TWITTER_ACCESS_TOKEN"),
+  access_token_secret: readEnvVariable("TWIR_TWITTER_ACCESS_TOKEN_SECRET"),
+  // bearer_token: readEnvVariable("TWIR_TWITTER_BEARER_TOKEN"),
 });
 
 function makeGet(endpoint, params = {}) {
@@ -26,11 +44,71 @@ function makeGet(endpoint, params = {}) {
   });
 }
 
-async function run() {
-  const result = await makeGet("https://ads-api.twitter.com/11/accounts");
-  // const result = await makeGet("/11/measurement/conversions/o85l6");
+function makePost(endpoint, params) {
+  return new Promise((resolve, reject) => {
+    client.post(endpoint, params, (error, data) => {
+      error ? reject(error) : resolve(data);
+    });
+  });
+}
 
-  console.log("result", result);
+async function run() {
+  console.log(
+    "Ads Accounts client1",
+    await makeGet("https://ads-api.twitter.com/11/accounts")
+  );
+  console.log("Ads Accounts client2", await client2.get("accounts"));
+
+  /*
+  const postConversionResult = await makePost(
+    "https://ads-api.twitter.com/11/measurement/conversions/o85l6",
+    {
+      conversions: [
+        {
+          conversion_time: "2023-01-23T00:00:00.000Z",
+          event_id: "tw-o85l6-od5nc",
+          identifiers: [
+            {
+              twclid: "23opevjt88psuo13lu8d020qkn",
+            },
+            {
+              hashed_email:
+                "d360d510a224510f373931ce2d6215a799f5a9c1cef221b0149b6b6b50cced62",
+            },
+          ],
+          // conversion_id: "TODO_USE_UUID_CK_OR_EMAIL_FOR_DEDUPLICATION ???",
+          description: "TWIR subscription",
+        },
+      ],
+    }
+  );
+  console.log("postConversionResult", postConversionResult);
+
+   */
+
+  const postConversionResult2 = await client2.post(
+    "measurement/conversions/o85l6",
+    {
+      conversions: [
+        {
+          conversion_time: "2023-01-23T00:00:00.000Z",
+          event_id: "tw-o85l6-od5nc",
+          identifiers: [
+            {
+              twclid: "23opevjt88psuo13lu8d020qkn",
+            },
+            {
+              hashed_email:
+                "d360d510a224510f373931ce2d6215a799f5a9c1cef221b0149b6b6b50cced62",
+            },
+          ],
+          // conversion_id: "TODO_USE_UUID_CK_OR_EMAIL_FOR_DEDUPLICATION ???",
+          description: "TWIR subscription",
+        },
+      ],
+    }
+  );
+  console.log("postConversionResult2", postConversionResult2);
 
   /*
   client.get(
@@ -62,7 +140,7 @@ async function run() {
   );
 
    */
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
 }
 
 run().then(
@@ -70,7 +148,7 @@ run().then(
     process.exit(0);
   },
   (e) => {
-    console.error("error", e);
+    console.error("catched run error", e);
     process.exit(1);
   }
 );
