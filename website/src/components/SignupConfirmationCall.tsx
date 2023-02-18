89ed954379032522537c794d6b2cb4724f4f612b
sebastienlorber <lorber.sebastien@gmail.com>
2023-02-18 18:14:28 +0100

    Add signup confirmation api call

--- website/src/components/SignupConfirmationCall.tsx
+++ website/src/components/SignupConfirmationCall.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect } from "react";
 import getAppendOnlySearch from "@site/src/utils/getAppendOnlySearch";
 
+function getSignupConfirmationQuery() {
+  return "test";
+  // return getAppendOnlySearch();
+}
+
 function useAPICall() {
   useEffect(() => {
-    const url = `/api/signup-confirmation?${getAppendOnlySearch()}`;
+    const url = `/api/signup-confirmation?${getSignupConfirmationQuery()}`;
     console.log("api call");
     fetch(url).then(console.log, console.error);
   }, []);
