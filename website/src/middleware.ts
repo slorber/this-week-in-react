e18e77fe64e859c4a9f3365d0148c4bbb6a7e5a7
sebastienlorber <lorber.sebastien@gmail.com>
2023-09-04 11:10:34 +0200

    add middleware test

---
+++ website/src/middleware.ts
@@ -0,0 +1,24 @@
+import {
+  next,
+  ipAddress,
+  geolocation,
+  type RequestContext,
+} from "@vercel/edge";
+
+// Vercel edge middleware
+// See https://vercel.com/docs/functions/edge-middleware
+
+export const config = {
+  matcher: "/", // TODO temp
+};
+
+export default function middleware(request: Request, context: RequestContext) {
+  const { country } = geolocation(request);
+  const ip = ipAddress(request);
+
+  if (country) {
+    console.log(`Vercel identified homepage visitor from country=${country}`);
+  }
+
+  return next();
+}
