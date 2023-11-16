496be759c428d3c5e4f19356f3ceac273af52846
sebastienlorber <lorber.sebastien@gmail.com>
2023-11-16 12:03:54 +0100

    remove useless posthog event

--- website/src/clientModules/posthog.ts
+++ website/src/clientModules/posthog.ts
@@ -3,5 +3,3 @@ import posthog from "posthog-js";
 posthog.init("phc_fxh8DHOkIdy8mnsuyHhJ9WuGEycEVY7EKAq21Qcd27q", {
   api_host: "https://eu.posthog.com",
 });
-
-posthog.capture("my event", { property: "value" });
