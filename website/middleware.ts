8ac59d75c75e79e6684dbc6cfd69bd8b85fe4f59
sebastien <lorber.sebastien@gmail.com>
2025-02-28 23:38:17 +0100

    middleware spam protection

--- website/middleware.ts
+++ website/middleware.ts
@@ -8,12 +8,33 @@ export const config = {
   matcher: "/", // TODO temp
 };
 
+const BlacklistedIps = [
+  "86.77.184.19", // TODO remove, it's me
+
+  // Ips from Feb 2025 fake subscriber attacks
+  "134.202.152",
+  "134.202.160",
+  "139.190.242",
+  "139.190.243",
+  "66.97.121",
+];
+
+function isBlacklistedIp(ip: string) {
+  return BlacklistedIps.some((blacklistedIp) => ip.startsWith(blacklistedIp));
+}
+
 export default function middleware(request: Request, context: RequestContext) {
   const { country } = geolocation(request);
   const ip = ipAddress(request);
 
   if (country) {
     console.log(`Vercel identified homepage visitor from country=${country}`);
+  }
+
+  if (isBlacklistedIp(ip)) {
+    return new Response("Spammer go to hell", {
+      status: 200,
+    });
   }
 
   return next();
