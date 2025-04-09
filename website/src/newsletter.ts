f0494beb94c77008d93d6f4c3ebc776511bf9da9
sebastien <lorber.sebastien@gmail.com>
2025-04-09 18:30:34 +0200

    229

--- website/src/newsletter.ts
+++ website/src/newsletter.ts
@@ -4,7 +4,7 @@ export const sponsorshipCalendarLink = "https://thisweekinreact.com/calendar";
 export const testimonialsLink = "https://thisweekinreact.com/testimonials";
 
 // TODO automate this!
-export const lastIssueLink = "/newsletter/228";
+export const lastIssueLink = "/newsletter/229";
 
 export type NewsletterSegment = {
   name: string;
